23de3bf90c00eab2f76f7c2b652bd077595b7337
LuceroSotelo <lucero.eds@gmail.com>
2021-11-12 10:56:56 -0500

    Agregué grit y mejoré css

--- src/main.js
+++ src/main.js
@@ -2,24 +2,25 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(data.results);
 
+let arrayPersonajes = data.results
+
 let botonVerTodos = document.getElementById("botonVerTodos");
 let botonIrFemale = document.getElementById("botonIrFemale");
 let botonIrMale = document.getElementById("botonIrMale");
 let botonIrUnkdown = document.getElementById("botonIrUnknown");
 let botonIrGenderless = document.getElementById("botonIrGenderless");
 
-
 //Mostrando vista de sección Ver Todos
 botonVerTodos.addEventListener("click", verVerTodos);
 function verVerTodos(){
-    document.getElementById("vistaPrincipal").style.display = "none";
+    document.getElementById("vistaInicio").style.display = "none";
     document.getElementById("verTodos").style.display = "block";
 }
 
 //Mostrando y ocultando vistas de Gender (sería mejor con if)
 botonIrFemale.addEventListener("click", verVistaFemale);
 function verVistaFemale(){
-    document.getElementById("vistaPrincipal").style.display = "none";
+    document.getElementById("vistaInicio").style.display = "none";
     document.getElementById("verTodos").style.display = "none";
     document.getElementById("vistaGenderFemale").style.display = "block";
     document.getElementById("vistaGenderMale").style.display = "none";
@@ -29,7 +30,7 @@ function verVistaFemale(){
 
 botonIrMale.addEventListener("click", verVistaMale);
 function verVistaMale(){
-    document.getElementById("vistaPrincipal").style.display = "none";
+    document.getElementById("vistaInicio").style.display = "none";
     document.getElementById("verTodos").style.display = "none";
     document.getElementById("vistaGenderFemale").style.display = "none";
     document.getElementById("vistaGenderMale").style.display = "block";
@@ -39,7 +40,7 @@ function verVistaMale(){
 
 botonIrUnkdown.addEventListener("click", verVistaUnknown);
 function verVistaUnknown(){
-    document.getElementById("vistaPrincipal").style.display = "none";
+    document.getElementById("vistaPrInicio").style.display = "none";
     document.getElementById("verTodos").style.display = "none";
     document.getElementById("vistaGenderFemale").style.display = "none";
     document.getElementById("vistaGenderMale").style.display = "none";
@@ -49,7 +50,7 @@ function verVistaUnknown(){
 
 botonIrGenderless.addEventListener("click", verVistaGenderless);
 function verVistaGenderless(){
-    document.getElementById("vistaPrincipal").style.display = "none";
+    document.getElementById("vistaInicio").style.display = "none";
     document.getElementById("verTodos").style.display = "none";
     document.getElementById("vistaGenderFemale").style.display = "none";
     document.getElementById("vistaGenderMale").style.display = "none";
@@ -57,15 +58,24 @@ function verVistaGenderless(){
     document.getElementById("vistaGenderGenderless").style.display = "block";
 }
 
-
-let arregloPersonajes = [];
-arregloPersonajes = data.results
-
-let newArray = arregloPersonajes.filter(function(element) {
-    return element.gender == "Female";    
+//Filtrando data por subcategoria Female
+const arrayFiltrado = arrayPersonajes.filter(function(subCategoria) {
+    return subCategoria.gender == "Female"    
 });
+
 console.log("=====Esta es la data filtrada por female======");
-console.log(newArray);
+console.log(arrayFiltrado);
+
+//Tomando objeto en la posición 0 del array de subcategoria Female
+let primerElemento = arrayFiltrado[0];
+//Monstrando el contenido del objeto
+let mostrarPrimerElemento = JSON.stringify(primerElemento);
+let introducirStringEnTarjeta = mostrarPrimerElemento.toString();
+console.log(introducirStringEnTarjeta);
 
 
- 
+
+let {a, b} = {a: 1, b: 2};
+console.log('a:', a, 'b:', b)
+
+
